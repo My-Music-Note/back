2cee689e42f0700a9d1eb5960bdd2bda2414e1d0
masiljangajji <xmfpdlsj0508@gmail.com>
2024-10-11 06:15:36 +0900

    feat: UserRepository #8

---
+++ src/main/java/my/music/note/back/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package my.music.note.back.user.repository;
+
+import my.music.note.back.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+    boolean existsByProviderId(String providerId);
+
+    User findByProviderId(String providerId);
+
+}
