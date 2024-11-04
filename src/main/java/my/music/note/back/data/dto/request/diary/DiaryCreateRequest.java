60a81bd2f127cb0a9db61218d0aa644a14e5498d
masiljangajji <xmfpdlsj0508@gmail.com>
2024-11-04 11:23:27 +0900

    feat: Diary Request DTO 추가 #16

---
+++ src/main/java/my/music/note/back/data/dto/request/diary/DiaryCreateRequest.java
@@ -0,0 +1,4 @@
+package my.music.note.back.data.dto.request.diary;
+
+public record DiaryCreateRequest(String content,Boolean isLongEntry) {
+}
---
+++ src/main/java/my/music/note/back/data/dto/request/diary/DiaryDeleteRequest.java
@@ -0,0 +1,4 @@
+package my.music.note.back.data.dto.request.diary;
+
+public record DiaryDeleteRequest(Long diaryId) {
+}
