85e9220877b58ffc997ce9b1c3345663f03165b4
masiljangajji <xmfpdlsj0508@gmail.com>
2024-12-04 07:39:37 +0900

    fix: RequestMapping 변경

--- src/main/java/my/music/note/back/controller/DiaryController.java
+++ src/main/java/my/music/note/back/controller/DiaryController.java
@@ -13,7 +13,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/api/diaries")
+@RestController
+@RequestMapping("/api/diaries")
 @RequiredArgsConstructor
 public class DiaryController {
 
