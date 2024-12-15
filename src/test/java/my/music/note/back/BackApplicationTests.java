0db4d4fa7e7c7b470c110418c0b969d20061a3be
masiljangajji <xmfpdlsj0508@gmail.com>
2024-12-15 17:03:29 +0900

    feat: DiaryServiceTest Lambda 호출 변경 및 ApplicationTest Context검증

--- src/test/java/my/music/note/back/BackApplicationTests.java
+++ src/test/java/my/music/note/back/BackApplicationTests.java
@@ -1,13 +1,21 @@
 package my.music.note.back;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class BackApplicationTests {
 
+    @Autowired
+    ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        assertThat(applicationContext).isNotNull();
     }
 
 }
